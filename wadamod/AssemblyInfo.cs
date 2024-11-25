06e99189158a2cdeb447fb6da37b200558248b8d
gl1tch638 <savrscum.mail.box@proton.me>
2024-11-25 02:08:49 -0500

    initial commit

---
+++ wadamod/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using MelonLoader;
+using wadamod; // The namespace of your mod class
+
+[assembly: MelonInfo(typeof(MyMod), "Wadality-provinground", "1.0.0", "gl1tch")]
+[assembly: MelonGame()]
+
+// add 2 melongame later "ManeraiLLC", "Wadality"
---
+++ wadamod/MyMod.cs
@@ -0,0 +1,32 @@
+﻿using MelonLoader;
+using Il2CppSirenix.OdinInspector;
+using UnityEngine;
+using Il2CppWadality.Core.SOTemplates;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using Il2CppSystem;
+
+namespace wadamod
+{
+    public class MyMod : MelonMod
+    {
+        
+        public override void OnInitializeMelon()
+        {
+			//     --Verify init--
+            LoggerInstance.Msg("WADAMOD V1 | GREETINGS FROM HELL, MICHIGAN");
+			//     --create SObjects--
+            MenuBoardPage modMenu = SerializedScriptableObject.CreateInstance<MenuBoardPage>();
+			SerializedScriptableObject wyNot = SerializedScriptableObject.CreateInstance<MenuBoardPage>();
+			WeaponMenu customRadial = SerializedScriptableObject.CreateInstance<WeaponMenu>();
+			GameMap inGameStomach = new GameMap();
+			inGameStomach.displayName = "Stomach";
+			inGameStomach.sceneName = "stomach";
+			
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            LoggerInstance.Msg($"Scene {sceneName} with build index {buildIndex} has been loaded!");
+        }
+    }
+}
